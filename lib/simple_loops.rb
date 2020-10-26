cbeb58ea32f1999c0b19ee2a84b611a7ff1ea1b6
Claire DeBoer <claire.deboer@gmail.com>
2020-10-26 20:29:38 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -8,17 +8,27 @@ end
 
 def loop_message_n_times (string, integer)
   count = 0
-  while count < limit
+  while count < integer do
     puts string
+    count += 1
   end
 end
 
 def output_array (array)
-  names.each do (element)
-  puts element
-end
+  count = 0
+  while count < array.length do
+    puts array[count]
+    count += 1
+  end
 end
 
 def return_string_array (array)
+  count = 0
+  new_array = []
+  while count < array.length do
+    new_array << array[count].to_s
+    count += 1
+  end
+  new_array
 end
 
